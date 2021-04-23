5c7f43157c40717a27a26fb2bb36f92cfdf5c94a
12Aleks <tanais2010@yahoo.com>
2021-04-23 13:16:34 +0200

    added files with error information

--- server/index.js
+++ server/index.js
@@ -4,16 +4,17 @@ const sequelize = require('./db')
 const models = require('./models/models')
 const cors = require('cors');
 const router = require('./routes/index')
-const PORT = process.env.PORT || 7000
+const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 
+const PORT = process.env.PORT || 7000
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/api', router)
-// app.get('/', (req, res) => {
-//   res.status(200).json({message: 'Working!!!' })
-// } )
+
+//Обязательно в конце этот middleware
+app.use(errorHandler)
 
 const start = async() => {
     try{
@@ -24,4 +25,4 @@ const start = async() => {
         console.log(e)
     }
 }
-start()
+start();
