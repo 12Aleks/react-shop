81505b338a2c46837a057bdc5ae9435b0bc2a6d9
12Aleks <tanais2010@yahoo.com>
2021-05-01 06:18:49 +0200

    added react-router and bootstrap, create routers

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,5 @@
 const ApiError = require('../error/ApiError')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt') //для хеширования паролей и чтоб они не были в открытом доступе
 const jwt = require('jsonwebtoken')//формирует токен
 const {User, Basket} = require('../models/models')
 
@@ -17,10 +17,12 @@ class UserController{
         if (!email || !password) {
             return next(ApiError.badRequest('Некорректный email или password'))
         }
+        //проверка на наличие email
         const candidate = await User.findOne({where: {email}})
         if (candidate) {
             return next(ApiError.badRequest('Пользователь с таким email уже существует'))
         }
+        //хеширование пароля с bcrypt
         const hashPassword = await bcrypt.hash(password, 5)
         const user = await User.create({email, role, password: hashPassword})
         const basket = await Basket.create({userId: user.id})
@@ -32,9 +34,11 @@ class UserController{
     async login(req, res, next){
           const {email, password} = req.body
 
+          // Ищим email в базе
           const user = await User.findOne({where: {email}})
           if(!user) return next(ApiError.badRequest('Taki użytkownik nie istnieje'));
 
+          // cравниваем пароль в базе и введенный пользователем
           let comparePassword = bcrypt.compareSync(password, user.password)
           if(!comparePassword) return next(ApiError.badRequest('Taki użytkownik nie istnieje'));
 
@@ -49,6 +53,8 @@ class UserController{
         //   return next(ApiError.badRequest('User dont have ID'))
         // }
         // res.json(id)
+        const token = generateJwt(req.user.id, req.user.email, req.user.role)
+        return  res.json({token})
     }
 }
 
---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,19 @@
+//декодируем токен и проверяем его на валидность
+const jwt = require('jsonwebtoken')
+
+module.exports = function (req, res, next) {
+   if(req.method === 'OPTIONS'){
+       next()
+   }
+   try{
+      const token = req.headers.authorization.split('')[1]
+       if(!token){{
+           return res.status(401).json({message: 'Uzytkownik nie jest autoryzowany'})
+       }}
+       const decoder = jwt.verify(token, process.env.SECRET_KEY);
+       req.user = decoder
+       next()
+   }catch(e){
+       res.status(401).json({message: 'Uzytkownik nie jest autoryzowany'})
+   }
+}
